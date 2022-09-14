6290efd685ed33233854efc30b4961cffe60cd27
GayathriAmarneni <gayathriamarneni@gmail.com>
2022-09-14 18:22:52 +0530

    remove the timer and changed to consumer template

--- src/main/java/com/example/camelconsumer/routes/RestConsumerRouter.java
+++ src/main/java/com/example/camelconsumer/routes/RestConsumerRouter.java
@@ -1,35 +1,40 @@
 package com.example.camelconsumer.routes;
 
 import org.apache.camel.CamelContext;
-import org.apache.camel.Exchange;
+import org.apache.camel.ConsumerTemplate;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.jackson.JacksonDataFormat;
+import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
 import com.example.camelconsumer.bean.Car;
 
+// "timer://test-rest-api?period=1000000"
 @Component
 public class RestConsumerRouter extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+        try (CamelContext context = new DefaultCamelContext();) {
+            ConsumerTemplate template = context.createConsumerTemplate();
+            context.start();
+            String car = template.receiveBody(
+                    "http://localhost:9090/api/car", String.class);
+            System.out.println(car);
 
-        from("timer://test-rest-api?period=1000000")
-                .log("Rest API calling...")
-                .setHeader(Exchange.HTTP_METHOD, simple("GET"))
-                .to("http://localhost:9090/api/car?outType= com.example.camelconsumer.bean.Car")
-                .unmarshal(new JacksonDataFormat(Car.class))
-                .process(new CamelProcessor());
+            context.stop();
+        } catch (Exception ex) {
+            System.out.println("Exception: " + ex);
+        }
 
+        Car car = new Car();
+        car.setCarName("Benz Maybach");
+        car.setCarModel("s-class");
+        car.setCompanyName("Mercedes");
         restConfiguration().component("servlet").bindingMode(RestBindingMode.json);
 
         rest("/car")
                 .get()
                 .to("direct:car");
-        Car car = new Car();
-        car.setCarName("Benz Maybach");
-        car.setCarModel("s-class");
-        car.setCompanyName("Mercedes");
         from("direct:car").transform().constant(car);
     }
 }
