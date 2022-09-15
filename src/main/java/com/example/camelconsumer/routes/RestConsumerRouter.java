ca08de85f9c6f4adf0b4f0f97c95e35f9db1c2f1
GayathriAmarneni <gayathriamarneni@gmail.com>
2022-09-15 11:21:08 +0530

    modified code

--- src/main/java/com/example/camelconsumer/routes/RestConsumerRouter.java
+++ src/main/java/com/example/camelconsumer/routes/RestConsumerRouter.java
@@ -6,7 +6,6 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
-import com.example.camelconsumer.bean.Car;
 
 // "timer://test-rest-api?period=1000000"
 @Component
@@ -21,20 +20,16 @@ public class RestConsumerRouter extends RouteBuilder {
                     "http://localhost:9090/api/car", String.class);
             System.out.println(car);
 
+            restConfiguration().component("servlet").bindingMode(RestBindingMode.json);
+
+            rest("/car")
+                    .get()
+                    .to("direct:car");
+            from("direct:car").transform().constant(car);
             context.stop();
         } catch (Exception ex) {
             System.out.println("Exception: " + ex);
         }
 
-        Car car = new Car();
-        car.setCarName("Benz Maybach");
-        car.setCarModel("s-class");
-        car.setCompanyName("Mercedes");
-        restConfiguration().component("servlet").bindingMode(RestBindingMode.json);
-
-        rest("/car")
-                .get()
-                .to("direct:car");
-        from("direct:car").transform().constant(car);
     }
 }
